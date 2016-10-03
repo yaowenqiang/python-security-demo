ff2a8aac4095396c2a9bf4bae5bd44673d70de1e
yaowenqiang <yaowenqiang111@163.com>
2016-10-04 02:24:32 +0800

    add thread demo

---
+++ Threads.py
@@ -0,0 +1,42 @@
+import threading
+
+class aThread(threading.Thread):
+    def __init__(self,num,val):
+        threading.Thread.__init__(self)
+        self.threadNum = num
+        self.loopCount = val
+
+    def run(self):
+        print("Starting run: ",self.threadNum)
+        myfunc(self.threadNum,self.loopCount)
+
+
+def myfunc(num,val):
+    count = 0
+    while count < val:
+        print(num," : ",val * count)
+        count = count +1
+
+t1 = aThread(1,15)
+t2 = aThread(2,20)
+t3 = aThread(3,25)
+t4 = aThread(1,15)
+
+# have to start them
+
+t1.start()
+t2.start()
+t3.start()
+t4.start()
+
+# could join them all into a collection
+threads = []
+threads.append(t1)
+threads.append(t2)
+threads.append(t3)
+threads.append(t4)
+
+# wait for all threads to complete by entering time
+
+for t in threads:
+    t.join()
