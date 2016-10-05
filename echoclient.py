c2fad1461a59c171ed88aef880f0119d3ca1ef53
yaowenqiang <yaowenqiang111@163.com>
2016-10-06 03:34:27 +0800

    add the echo client demo

---
+++ echoclient.py
@@ -0,0 +1,26 @@
+from twisted.internet import reactor,protocol
+class EchoClient(protocol.Protocol):
+    def connectionMade(self):
+        self.transport.write(b'hello world!')
+
+    def dataReceived(self,data):
+        print('Server said:',data)
+        self.transport.loseConnection()
+
+    def connectionLost(self,reason):
+        print('connection lost')
+
+class EchoFactory(protocol.ClientFactory):
+    protocol = EchoClient
+
+    def clientConnectionFailed(self,connector,reason):
+        print('Connection failed - goodbye!')
+        reactor.stop()
+
+def main():
+    f = EchoFactory()
+    reactor.connectTCP('localhost',8000,f)
+    reactor.run()
+
+if __name__ == '__main__':
+    main()
